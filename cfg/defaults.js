b794bcd4056aed2adf406b28fb8631f882c6d77e
Michael Berman <me@scripti.st>
2016-09-07 21:49:00 +1000

    Yaml front matter loader

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -38,6 +38,10 @@ function getDefaultModules() {
         test: /\.(png|jpg|gif|woff|woff2)$/,
         loader: 'url-loader?limit=8192',
       },
+      {
+        test: /\.txt$/,
+        loader: 'json-loader!yaml-frontmatter-loader',
+      },
     ],
   };
 }
