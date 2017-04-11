f3d3785520603ca5b898cbbfe771f05065b3cfbc
Michael Berman <me@scripti.st>
2017-04-11 14:31:31 +1000

    Automatically load all data

--- src/data/index.js
+++ src/data/index.js
@@ -1,65 +1,13 @@
-/* eslint-disable quote-props,object-shorthand */
+const context = require.context('./', true, /\.(txt)$/);
+const songs = {};
 
-import aWholeNewWorld from './a-whole-new-world.txt';
-import allIWantForChristmasIsYou from './all-i-want-for-christmas-is-you.txt';
-import annieWaits from './annie-waits.txt';
-import bard from './bard.txt';
-import darkSide from './dark-side.txt';
-import dontStopBelieving from './dont-stop-believing.txt';
-import enormousPenis from './enormous-penis.txt';
-import fourChordsSong from './four-chords-song.txt';
-import fredJonesPart2 from './fred-jones-part-2.txt';
-import friendLikeMe from './friend-like-me.txt';
-import goodbyeYellowBrickRoad from './goodbye-yellow-brick-road.txt';
-import iDontLikeMondays from './i-dont-like-mondays.txt';
-import inSummer from './in-summer.txt';
-import landed from './landed.txt';
-import letItGo from './let-it-go.txt';
-import majesty from './majesty.txt';
-import myGirlfriendWhoLivesInCanada from './my-girlfriend-who-lives-in-canada.txt';
-import notPerfect from './not-perfect.txt';
-import skullcrusherMountain from './skullcrusher-mountain.txt';
-import songForPhilDaoust from './song-for-phil-daoust.txt';
-import spaceOddity from './space-oddity.txt';
-import theLuckiest from './the-luckiest.txt';
-import theSwan from './the-swan.txt';
-import totalEclipseOfTheHeart from './total-eclipse-of-the-heart.txt';
-import vincent from './vincent.txt';
-import waterloo from './waterloo.txt';
-import youDontKnowMe from './you-dont-know-me.txt';
-import youllBeBack from './youll-be-back.txt';
-import yourSong from './your-song.txt';
-import youveGotAFriendInMe from './youve-got-a-friend-in-me.txt';
+context.keys().forEach((filename) => {
+  const slug = filename.match(/^\.\/(.*)\.txt$/)[1];
+  if (!slug) {
+    throw new Error(`Could not build slug for file ${filename}`);
+  } else {
+    songs[slug] = context(filename);
+  }
+});
 
-export default {
-  'a-whole-new-world': aWholeNewWorld,
-  'all-i-want-for-christmas-is-you': allIWantForChristmasIsYou,
-  'annie-waits': annieWaits,
-  'bard': bard,
-  'dark-side': darkSide,
-  'dont-stop-believing': dontStopBelieving,
-  'enormous-penis': enormousPenis,
-  'four-chords-song': fourChordsSong,
-  'fred-jones-part-2': fredJonesPart2,
-  'friend-like-me': friendLikeMe,
-  'goodbye-yellow-brick-road': goodbyeYellowBrickRoad,
-  'i-dont-like-mondays': iDontLikeMondays,
-  'in-summer': inSummer,
-  'landed': landed,
-  'let-it-go': letItGo,
-  'majesty': majesty,
-  'my-girlfriend-who-lives-in-canada': myGirlfriendWhoLivesInCanada,
-  'not-perfect': notPerfect,
-  'skullcrusher-mountain': skullcrusherMountain,
-  'song-for-phil-daoust': songForPhilDaoust,
-  'space-oddity': spaceOddity,
-  'the-luckiest': theLuckiest,
-  'the-swan': theSwan,
-  'total-eclipse-of-the-heart': totalEclipseOfTheHeart,
-  'vincent': vincent,
-  'waterloo': waterloo,
-  'you-dont-know-me': youDontKnowMe,
-  'youll-be-back': youllBeBack,
-  'your-song': yourSong,
-  'youve-got-a-friend-in-me': youveGotAFriendInMe,
-};
+export default songs;
