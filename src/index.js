5b90a87b5879516e2639383402e33ea89b88b9c8
Michael Berman <me@scripti.st>
2017-04-24 18:50:50 +1000

    Browser history instead of hash history

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/App';
 import SongPage from './components/SongPage';
@@ -13,7 +13,7 @@ function scrollToTop() {
 
 // Render the main component into the dom
 ReactDOM.render((
-  <Router history={hashHistory} onUpdate={scrollToTop}>
+  <Router history={browserHistory} onUpdate={scrollToTop}>
     <Route path="/" component={App}>
       {songs.map(song => (
         <Route key={song.slug} path={song.slug} component={SongPage} song={song} />
