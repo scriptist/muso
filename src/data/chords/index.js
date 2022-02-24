a32f3e2a31a7f866f1536cfa821348e70a2dad6f
Mike Berman <mike@berman.xyz>
2022-02-24 07:43:41 +0000

    Init

---
+++ src/data/chords/index.js
@@ -0,0 +1,13 @@
+const context = require.context('./', true, /\.(txt)$/);
+const songs = {};
+
+context.keys().forEach((filename) => {
+  const slug = filename.match(/^\.\/(.*)\.txt$/)[1];
+  if (!slug) {
+    throw new Error(`Could not build slug for file ${filename}`);
+  } else {
+    songs[slug] = context(filename);
+  }
+});
+
+export default songs;
